5fecc8c8363e0d529dcd34d3f4d62e43a41c5bd2
Kleby <kleby10@gmail.com>
2022-11-10 11:17:05 -0300

    adicionando os projetos iniciais da IMERSAO

---
+++ pages/index.js
@@ -0,0 +1,94 @@
+import { Children } from "react";
+import styled from "styled-components";
+import config from "../config.json";
+import { CSSReset } from "../src/componets/CSSReset";
+import Menu from "../src/componets/Menu.js";
+import { StyledTimeline } from "../src/componets/Timeline";
+
+const HomePage = () => {
+  const styleOfPage = { 
+    // backgroundColor: "red" 
+  };
+
+  return (
+    <>
+      <CSSReset />
+      <div style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          // backgroundColor: "red",
+        }}
+      >
+        <Menu />
+        <Header />
+        <TimeLine playlist={config.playlists}>{Children}</TimeLine>
+      </div>
+    </>
+  );
+};
+
+// const Menu = () => {
+//   return <div>Menu</div>;
+// };
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <img src="banner" />
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
+};
+
+const TimeLine = (props) => {
+  const playlistVideos = Object.keys(props.playlist);
+  //Statment
+  //Retorno pro expressão
+  return (
+    <StyledTimeline>
+      {playlistVideos.map((playlistNames) => {
+        const videos = props.playlist[playlistNames];
+        return (
+          <section>
+            <h2>{playlistNames}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>{video.title}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
+};
+
+export default HomePage;
